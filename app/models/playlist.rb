147ae8b07369493e6d090b9c462726542159f637
Ilya Obretetskiy <obreil54@gmail.com>
2025-03-27 12:15:15 +0000

    Added the playlist track model with relevant unit tests

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,5 +1,7 @@
 class Playlist < ApplicationRecord
   belongs_to :user
+  has_many :playlist_tracks, dependent: :destroy
+  has_many :tracks, through: :playlist_tracks
 
   validates :name, presence: true
   validates :name, uniqueness: {scope: :user_id, message: "You already have a playlist with this name"}
